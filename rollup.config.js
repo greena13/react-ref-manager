13cae8a6c98f80e9cb2c1ae4252cc9fb09498b8a
Aleck Greenham <greenhama13@gmail.com>
2018-02-10 12:50:08 +0000

    Add license file

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
+import license from 'rollup-plugin-license';
+import path from 'path';
 
 export default {
   input: 'src/index.js',
+
   output: {
     format: 'cjs',
     file: process.env.NODE_ENV === 'production' ? 'cjs/react-ref-manager.production.min.js' : 'cjs/react-ref-manager.development.js',
@@ -20,6 +23,12 @@ export default {
       ENV: JSON.stringify(process.env.NODE_ENV || 'development')
     }),
 
-    (process.env.NODE_ENV === 'production' && uglify())
+    (process.env.NODE_ENV === 'production' && uglify()),
+
+    license({
+      banner: {
+        file: path.join(__dirname, 'LICENSE'),
+      }
+    })
   ]
 };
